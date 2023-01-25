ac78216c955e323b820d89317b5032d112c92854
THERMOMETER <gijs.timmerij@gmail.com>
2023-01-25 13:25:43 +0100

    model save and load

--- model.py
+++ model.py
@@ -2,13 +2,14 @@ from tensorflow import keras
 
 
 
-def get_weather(location, date):
+def get_weather_data(centroid, date):
     return
 
-def get_sattelite_data(location, date):
+def get_sattelite_data(location_name, date):
     return
 
-def create_sl
+def create_sliding_window(data):
+    return
 
 def get_model():
     return keras.models.load_model('tcn_ndvi.h5')
